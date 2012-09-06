2c65b3b97de164fc1dc856ae830e4366ddf01e6a
Emerson MX <emersonmx@gmail.com>
2012-09-06 14:31:44 -0300

    No lugar de usar apenas um leitor, resolvi definir uma classe tanto para leitura quanto escrita.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- src/turing_machine/machine.py
+++ src/turing_machine/machine.py
@@ -1,17 +1,29 @@
+# -*- coding: utf8 -*-
+
 class Machine(object):
-    def __init__(self, states, input_alphabet, tape_alphabet,
+    LEFT = 1
+    STOP = 0
+    RIGHT = -1
+
+    def __init__(self, input_data, states, input_alphabet, tape_alphabet,
                  transition_function, start_state, accept_state, reject_state):
+
+        if not input_data:
+            input_data = ' '
 
         self.states = states
         self.input_alphabet = input_alphabet
         self.tape_alphabet = tape_alphabet
         self.transition_function = transition_function
-        self.start_state = start_state
+        self.current_state = start_state
         self.accept_state = accept_state
         self.reject_state = reject_state
 
-        self.tape = []
-        self.tape_head = 0
+        self.tape = list(' ' + input_data + ' ')
+        self.tape_head = 1
+
+        # Checar entradas e lançar exceções (ver definição formal no livro).
 
     def run(self):
         pass
+
---
+++ src/turing_machine/serializer.py
@@ -0,0 +1,7 @@
+class Serializer(object):
+    def load(self, file_name):
+        pass
+
+    def save(self, data, file_name):
+        pass
+
